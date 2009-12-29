3b8ba7174053f4201554f393b281db88c139f160
Karol Krizka <kkrizka@gmail.com>
2009-12-29 14:31:01 -0800

    - Reorganized code - Build libmidireader library - Added CMake as a build tool

--- Makefile
+++
@@ -1,9 +0,0 @@
-OBJS=main.o MIDIFile.o MIDIChunk.o MIDIHeader.o MIDITrack.o MIDIEvent.o MIDIChannelEvent.o MIDIChannelControllerEvent.o MIDIMetaEvent.o MIDIMetaGenericEvent.o MIDIMetaTextEvent.o MIDIMetaNumberEvent.o types.o
-
-all: ${OBJS}
-	g++ -g -o midireader ${OBJS}
-
-clean:
-	rm -f midireader
-	rm -f ${OBJS}
-	rm -f *~ \#*
---
+++ src/MIDIReader.h
@@ -0,0 +1,23 @@
+#ifndef MIDIREADER_H_
+#define MIDIREADER_H_
+
+#include "MIDIDefines.h"
+
+#include "MIDIFile.h"
+
+#include "MIDIChunk.h"  
+#include "MIDIHeader.h"
+#include "MIDITrack.h"
+
+#include "MIDIEvent.h"
+
+#include "MIDIChannelEvent.h" 
+#include "MIDIChannelControllerEvent.h"
+
+#include "MIDIMetaEvent.h" 
+#include "MIDIMetaGenericEvent.h"
+#include "MIDIMetaTextEvent.h"
+#include "MIDIMetaNumberEvent.h" 
+
+
+#endif // MIDIREADER_H_
